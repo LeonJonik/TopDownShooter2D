e3b01b1b6ef59f7bd7428608656b5884f5b8f2dc
LeonJonik <leon.jonikic@elev.ga.lbs.se>
2024-10-18 12:56:57 +0200

    Added project

---
+++ Top Down Shooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] Transform player;
+    [SerializeField] float enemySpeed;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.MovePosition(rb.position + direction * enemySpeed * Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ Top Down Shooter/Assets/Scripts/Player/PlayerDamage.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour
+{
+    [SerializeField] int playerHealth = 5;
+    [SerializeField] float invicibilityTime = 2f;
+    bool invincible = false;
+
+    void DisableInvincibility()
+    {
+        invincible = true;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (invincible == true)
+            {
+                return;
+            }
+            if (playerHealth > 0)
+            {
+                playerHealth--;
+                invincible = true;
+                Invoke("DisableInvincibility", invicibilityTime);
+                Debug.Log("Player's Health: " + playerHealth);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
